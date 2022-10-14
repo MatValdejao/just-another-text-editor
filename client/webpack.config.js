9b9ce1ae5e4b5c271ac6442a169c547f6703f572
MatValdejao <matvaldejao123@gmail.com>
2022-10-13 20:45:46 -0400

    added webpack plugins and rules

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,23 +8,65 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
+		mode: "development",
+		entry: {
+			main: "./src/js/index.js",
+			install: "./src/js/install.js",
+		},
+		output: {
+			filename: "[name].bundle.js",
+			path: path.resolve(__dirname, "dist"),
+		},
+		plugins: [
+			// html plugin to inject bundles
+			new HtmlWebpackPlugin({
+				template: "./index.html",
+				title: "Jate",
+			}),
+			// service-worker
+			new InjectManifest({
+				swSrc: "./src-sw.js",
+				swDest: "src-sw.js",
+			}),
+			// manifest.json
+			new WebpackPwaManifest({
+				inject: true,
+				name: "Text Editor",
+				short_name: "Jate",
+				description: "Keep track of texts!",
+				// background_color: "#7eb4e2",
+				// theme_color: "#7eb4e2",
+				start_url: "./",
+				publicPath: "./",
+				icons: [
+					{
+						src: path.resolve("src/images/logo.png"),
+						sizes: [96, 128, 192, 256, 384, 512],
+						destination: path.join("assets", "icons"),
+					},
+				],
+			}),
+		],
 
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
+		module: {
+			rules: [
+				// css loader
+				{
+					test: /\.css$/i,
+					use: ["style-loader", "css-loader"],
+        },
+        // babel
+				{
+					test: /\.m?js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: "babel-loader",
+						options: {
+							presets: [["@babel/preset-env", { targets: "defaults" }]],
+						},
+					},
+				},
+			],
+		},
+	};
 };
